ec4fbde5deb39c8b8a12a1be484fb5e499ec4594
Luka <f1sloppy@gmail.com>
2024-03-03 14:54:07 -0300

    FichaGerador

---
+++ FichaGerador/func.py
@@ -0,0 +1,13 @@
+pv = defesa = pm = 0
+
+def pv_calculo(a, b, c):
+    pv = a + b + c
+    return pv
+
+def defesa_calculo(a, b, c, d, e):
+    defesa = a + b + c + d + e
+    return defesa
+
+def pm_calculo(a, b):
+    pm = a + b
+    return pm
